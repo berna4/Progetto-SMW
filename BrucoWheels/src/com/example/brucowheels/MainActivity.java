1b0bdbbd47bcb884d01ec8f7ef4708cbb8179cee
Matteo Bernardon <matteo.bernardon@gmail.com>
2015-06-19 10:30:51 +0200

    Aggiunto progetto

---
+++ BrucoWheels/src/com/example/brucowheels/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.brucowheels;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity implements OnClickListener, PeerListListener {
+
+	private WifiP2pManager mManager;
+	private Channel mChannel;
+	private BroadcastReceiver mReceiver;
+	private IntentFilter mIntentFilter;
+    private List peers = new ArrayList();
+    private ArrayList<String> peersName = new ArrayList<String>();
+    	
+    @Override
+    public void onPeersAvailable(WifiP2pDeviceList peerList) {
+
+        // Out with the old, in with the new.
+        peers.clear();
+        peers.addAll(peerList.getDeviceList());
+
+        // If an AdapterView is backed by this data, notify it
+        // of the change.  For instance, if you have a ListView of available
+        // peers, trigger an update.
+        //((ListAdapter) getListAdapter()).notifyDataSetChanged();
+        if (peers.size() == 0) {
+        	System.out.println("i nomi sono: ");
+        	Toast.makeText(getApplicationContext(), "Non c'è nu gaz", Toast.LENGTH_LONG).show();
+        }
+    }
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	    mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+	    mChannel = mManager.initialize(this, getMainLooper(), null);
+	    mReceiver = new WifiReceiver(mManager, mChannel, this);
+	    mIntentFilter = new IntentFilter();
+	    mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+	    mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+	    mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+	    mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+	    
+	    Button b = (Button) this.findViewById(R.id.searcher);
+		b.setOnClickListener(this);
+	}
+	
+	/* register the broadcast receiver with the intent values to be matched */
+	@Override
+	protected void onResume() {
+	    super.onResume();
+	    registerReceiver(mReceiver, mIntentFilter);
+	}
+	/* unregister the broadcast receiver */
+	@Override
+	protected void onPause() {
+	    super.onPause();
+	    unregisterReceiver(mReceiver);
+	}
+	
+	public void onClick (View v) {
+		searchDevices();		
+	}
+	
+	private void searchDevices() {
+		mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+		    @Override
+		    public void onSuccess() {
+		    	
+		    }
+
+		    @Override
+		    public void onFailure(int reasonCode) {
+		    	
+		    }
+		});
+		getDeviceName();
+		Intent intent = new Intent(this, ListDevices.class);
+		intent.putStringArrayListExtra("deviceList", peersName);
+		//startActivity(intent);
+	}
+	
+	private void getDeviceName() {
+		Iterator<WifiP2pDevice> it = peers.iterator();
+		while(it.hasNext()) {
+			peersName.add(it.next().deviceName);
+			
+			System.out.println(it.next().deviceName);
+		}	
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
